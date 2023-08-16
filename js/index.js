16d4e6982054998237746a0d774f62fd1dcab6bb
SKodehode <stiankodehode@gmail.com>
2023-08-16 15:50:54 +0200

    Updated the JS document. It now creates pokemon cards dynamically.

--- js/index.js
+++ js/index.js
@@ -4,12 +4,7 @@ const sortByName = document.getElementById("btn-sortbyname");
 const sortByType = document.getElementById("btn-sortbytype");
 const sortByNumber = document.getElementById("btn-sortbyNumber");
 const cardContainer = document.getElementById("card-container");
-const itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
 
-/* https://pokeapi.co/api/v2/pokemon?limit=151&offset=0 */
-/* Official artwork https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/(1-151).png */
-
-/* Need to collect name, id, type slot 1 and type slot 2 */
 
 function catchPokemon() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
@@ -19,8 +14,7 @@ function catchPokemon() {
                 fetchPokemonData(pokemon);
             })
         })
-    }
-
+}
 catchPokemon()
 
 
@@ -29,71 +23,54 @@ function fetchPokemonData(pokemon){
     fetch(url)
     .then(response => response.json())
     .then(function(pokeData){
-        console.log(pokeData)
-    })
-}
-
-
-
-const paragraph = document.createElement("p")
-paragraph.textContent = "This is a test"
-
-cardContainer.appendChild(paragraph)
-
-const text = document.createTextNode(" -this is also a test",);
-
-const linkPara = document.querySelector("p");
-linkPara.appendChild(text);
-
-console.log(itemsArray)
-
-
-const myObjectArray = [
-    { name: "Bulbasaur", id: 1, officialartwork: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png", type1: "grass", type2: "poison"},
-    { name: "Charmander", id: 2, officialartwork: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png", type1: "Fire", type2: "Dragon"}
-];
-
-for (const obj of myObjectArray){
-
-    const pokemonCard = document.createElement("div");
-    pokemonCard.id = "pokemon-card";
-    cardContainer.appendChild(pokemonCard);
+        const pokeName = pokeData.name;
+        const pokeNumber = pokeData.id;
+        const pokeType1 = pokeData.types[0].type.name;
+        const pokeType2 = pokeData.types[1] ? pokeData.types[1].type.name : null;
+        const pokeImage = pokeData.sprites.other["official-artwork"].front_default;
+        
+        const pokemonCard = document.createElement("div");
+        pokemonCard.id = "pokemon-card";
+        cardContainer.appendChild(pokemonCard);
+        
+        const pokemonImage = document.createElement("img");
+        pokemonImage.id = "pokemonImage";
+        pokemonImage.src = pokeImage;
+        pokemonCard.appendChild(pokemonImage);
     
-    const pokemonImage = document.createElement("img");
-    pokemonImage.id = "pokemonImage";
-    pokemonImage.src = obj.officialartwork;
-    pokemonCard.appendChild(pokemonImage);
-
-    const pokemonNumber = document.createElement("p");
-    pokemonNumber.id = "pokemon-number";
-    pokemonNumber.textContent = "#" + obj.id;
-    pokemonCard.appendChild(pokemonNumber);
-
-    const cardInfo = document.createElement("div");
-    cardInfo.id = "card-info";
-    pokemonCard.appendChild(cardInfo);
-
-    const pokemonName = document.createElement("h1")
-    pokemonName.id = "pokemon-name";
-    pokemonName.textContent = obj.name;
-    cardInfo.appendChild(pokemonName);
-
-    const pokemonTypeTitle = document.createElement("h3");
-    pokemonTypeTitle.id = "type-title";
-    pokemonTypeTitle.textContent = "Type:"
-    cardInfo.appendChild(pokemonTypeTitle)
-
-    const pokemonType = document.createElement("div");
-    pokemonType.id = "pokemon-type-1";
-    cardInfo.appendChild(pokemonType);
-
-    const pokemonType1 = document.createElement("p");
-    pokemonType1.id = "pokemon-type-1";
-    pokemonType1.textContent = obj.type1;
-    pokemonType.appendChild(pokemonType1);
-
-    const pokemonType2 = document.createElement("p");
-    pokemonType2.id = "pokemon-type-1";
-    pokemonType2.textContent = obj.type2;
-    pokemonType.appendChild(pokemonType2);
-}
+        const pokemonNumber = document.createElement("p");
+        pokemonNumber.id = "pokemon-number";
+        pokemonNumber.textContent = "#" + pokeNumber;
+        pokemonCard.appendChild(pokemonNumber);
+    
+        const cardInfo = document.createElement("div");
+        cardInfo.id = "card-info";
+        pokemonCard.appendChild(cardInfo);
+    
+        const pokemonName = document.createElement("h1")
+        pokemonName.id = "pokemon-name";
+        pokemonName.textContent = pokeName;
+        cardInfo.appendChild(pokemonName);
+    
+        const pokemonTypeTitle = document.createElement("h3");
+        pokemonTypeTitle.id = "type-title";
+        pokemonTypeTitle.textContent = "Types:"
+        cardInfo.appendChild(pokemonTypeTitle)
+    
+        const pokemonType = document.createElement("div");
+        pokemonType.id = "pokemon-type-1";
+        cardInfo.appendChild(pokemonType);
+    
+        const pokemonType1 = document.createElement("p");
+        pokemonType1.id = "pokemon-type-1";
+        pokemonType1.textContent = pokeType1;
+        pokemonType.appendChild(pokemonType1);
+    
+        if (pokeType2) {
+            const pokemonType2 = document.createElement("p");
+            pokemonType2.id = "pokemon-type-1";
+            pokemonType2.textContent = pokeType2;
+            pokemonType.appendChild(pokemonType2);
+        }
+    })
+};
